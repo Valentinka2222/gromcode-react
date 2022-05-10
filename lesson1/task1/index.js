a2b66c200f1538fd54492e67e7d9a5b364e3cd03
Valentinka2222 <xamalyan89@gmail.com>
2022-05-10 19:28:53 +0300

    space

--- lesson1/task1/index.js
+++ lesson1/task1/index.js
@@ -2,6 +2,6 @@ const rootElement = document.querySelector('#root');
 
 const greetingElement = React.createElement('div', {
     className: 'greeting'
-},'Hello,React!'
+},'Hello, React!'
 )
 ReactDOM.render(greetingElement,rootElement)
