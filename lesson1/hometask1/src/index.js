375b7d26a196405220fd18df038e2e8f5f31c23b
Valentinka2222 <xamalyan89@gmail.com>
2022-05-11 13:12:52 +0300

    L1h1

---
+++ lesson1/hometask1/src/index.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './styles.css';
+
+const todoListElem = document.querySelector('#root')
+const element = 
+    (<h1 className='title'>Todo List</h1>)
+    
+ReactDOM.render(element,todoListElem)
