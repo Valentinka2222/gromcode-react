aa8b39970e1ff4d473877c7e47ea1abc964a7047
Valentinka2222 <xamalyan89@gmail.com>
2022-05-12 18:43:19 +0300

    L2t2

--- lesson2/task1/src/index.js
+++ lesson2/task1/src/index.js
@@ -6,11 +6,13 @@ import ReactDOM from 'react-dom';
 const rootElement = document.querySelector('#root')
 
 const element = [
+
     < h1 > Search Form</h1 >,
-<div>
-    <input type="text"></input>
-    <button>Search</button>
-</div>]
+    <div>
+      <input type="text"></input>
+      <button>Search</button>
+    </div>
+ ]
 
 
 
