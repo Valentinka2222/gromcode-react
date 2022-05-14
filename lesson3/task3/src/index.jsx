855b28c6e25db81d45d87687fd49d861bb2f08a6
Valentinka2222 <xamalyan89@gmail.com>
2022-05-15 00:30:06 +0300

    L3t3

---
+++ lesson3/task3/src/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Comment from "./Comment"
+import './styles.scss'
+import ReactDOM from 'react-dom';
+
+const rootelement = document.querySelector('#root')
+
+const userInfo = {
+    name: "Tom",
+    avatarUrl:"https://avatars.githubusercontent.com/u/69631?s=200&v=4"
+}
+
+
+ReactDOM.render(<Comment
+    user={userInfo}
+    text='Good Job'
+    date ={new Date()}
+/>, rootelement)
+
+
