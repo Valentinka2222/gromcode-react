0eade7b77d7cbf2946d6dc4857bdaa683cd21b04
Valentinka2222 <xamalyan89@gmail.com>
2022-05-14 23:24:59 +0300

    L3t2

---
+++ lesson3/task2/src/index.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import SearchField from './Search.jsx'
+
+import ReactDOM from 'react-dom';
+
+import './index.scss'
+import './search.scss'
+
+const rootelement = document.querySelector('#root')
+
+
+ReactDOM.render(<SearchField name="Tom"/>, rootelement)
