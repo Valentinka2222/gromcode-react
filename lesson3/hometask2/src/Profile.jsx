48ee2a8b40e88ee098feacbb98d3bfdd291bebdf
Valentinka2222 <xamalyan89@gmail.com>
2022-05-15 20:06:51 +0300

    L3ht2

---
+++ lesson3/hometask2/src/Profile.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import moment from "moment"
+
+
+const formatDate = date => moment(date).format("DD MMM YY");
+
+
+
+const Profile = (props) => {
+    return ([<div className="greeting">
+       <span className="profile__name">{props.firstName} {props.lastName}</span>
+       <span className="profile__birth">Was born {formatDate(props.birthDate)} in {props.birthPlace}</span>
+    </div>])   
+}
+export default Profile
+
---
+++ lesson3/hometask2/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import App from "./App"
+import './index.scss'
+import ReactDOM from 'react-dom';
+
+
+const rootelement = document.querySelector('#root')
+ReactDOM.render(<App/>,rootelement)
