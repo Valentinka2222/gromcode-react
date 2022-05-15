540293e4675ed2abc2169679fa6ebdd449fe3dc9
Valentinka2222 <xamalyan89@gmail.com>
2022-05-15 20:26:04 +0300

    2

--- lesson3/hometask2/src/Profile.jsx
+++ lesson3/hometask2/src/Profile.jsx
@@ -8,9 +8,9 @@ const formatDate = date => moment(date).format("DD MMM YY");
 
 const Profile = (props) => {
     return ([<div className='greeting'>
-       <span className="profile__name">{props.firstName} {props.lastName}</span>
+       <span className="profile__name">{props.firstName} {props.lastName}.</span>
        <span className="profile__birth"> Was born {formatDate(props.birthDate)} in {props.birthPlace}</span>
-   </div> ])   
+   </div>])   
 }
 export default Profile
 
