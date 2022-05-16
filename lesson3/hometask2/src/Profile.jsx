cd8bf463508751ef01430c884c9ef2b0e49c76d0
Valentinka2222 <xamalyan89@gmail.com>
2022-05-16 13:43:06 +0300

    2

--- lesson3/hometask2/src/Profile.jsx
+++ lesson3/hometask2/src/Profile.jsx
@@ -1,29 +1,26 @@
 import React from 'react';
 import moment from "moment"
 
-
-
-
-
 const userData = {
   firstName: 'James',
   lastName: 'Bond',
   birthDate: '1991-01-17T11:11:11.819Z',
   birthPlace: 'London',
 };
-export const App = () => {
+/*export const App = () => {
     return(<Profile
         firstName={userData.firstName}
         lastName={userData.lastName}
         birthDate={userData.birthDate}
         birthPlace={userData.birthPlace}
 />)
-}
+}*/
 const formatDate = date => moment(date).format("DD MMM YY");
 
 
 
 const Profile = (props) => {
+    props=userData
     return (<div className='greeting'>
         <span className="profile__name">{`${props.firstName} ${props.lastName}`}</span><br></br>
        <span className="profile__birth">{`Was born ${formatDate(props.birthDate)} in ${props.birthPlace}`}</span>
--- lesson3/hometask2/src/index.jsx
+++ lesson3/hometask2/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { App } from "./Profile"
+import Profile, { App } from "./Profile"
 import './index.scss'
 import ReactDOM from 'react-dom';
 
 
 const rootElement = document.querySelector('#root')
-ReactDOM.render(<App/>,rootElement)
+ReactDOM.render(<Profile/>,rootElement)
