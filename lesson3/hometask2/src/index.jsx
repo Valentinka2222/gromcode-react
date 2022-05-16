31ca2f7949677c1baff0b53f543c954b73c909e5
Valentinka2222 <xamalyan89@gmail.com>
2022-05-16 14:37:27 +0300

    6

--- lesson3/hometask2/src/index.jsx
+++ lesson3/hometask2/src/index.jsx
@@ -3,8 +3,16 @@ import Profile  from "./Profile"
 import './index.scss'
 import ReactDOM from 'react-dom';
 
+const userData = {
+  firstName: 'James',
+  lastName: 'Bond',
+  birthDate: '1991-01-17T11:11:11.819Z',
+  birthPlace: 'London',
+};
+
+
 const rootElement = document.querySelector('#root')
-ReactDOM.render(<Profile   firstName= 'James'
-  lastName= 'Bond'
-  birthDate= '1991-01-17T11:11:11.819Z'
-  birthPlace= 'London'></Profile>,rootElement)
+ReactDOM.render(<Profile   firstName ={userData.firstName}
+  lastName={userData.lastName}
+  birthDate= {userData.birthDate}
+  birthPlace= {userData.birthPlace} > </Profile>,rootElement)
