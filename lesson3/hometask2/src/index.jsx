0410f3ab268fe3cfd1f1acfac149f6038d87e468
Valentinka2222 <xamalyan89@gmail.com>
2022-05-16 14:27:31 +0300

    8

--- lesson3/hometask2/src/index.jsx
+++ lesson3/hometask2/src/index.jsx
@@ -3,6 +3,8 @@ import Profile  from "./Profile"
 import './index.scss'
 import ReactDOM from 'react-dom';
 
-
 const rootElement = document.querySelector('#root')
-ReactDOM.render(<Profile/>,rootElement)
+ReactDOM.render(<Profile   firstName= 'James'
+  lastName= 'Bond'
+  birthDate= '1991-01-17T11:11:11.819Z'
+  birthPlace= 'London'></Profile>,rootElement)
