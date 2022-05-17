1f6f1e92aaf20e841a41f4cf676aa841cbdb0484
Valentinka2222 <xamalyan89@gmail.com>
2022-05-17 19:21:19 +0300

    L5t2

---
+++ lesson5/task2/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import GoodButton from './GoodButton';
+
+const rootelement = document.querySelector('#root');
+
+ReactDOM.render(<GoodButton />, rootelement);
