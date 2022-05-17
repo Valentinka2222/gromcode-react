f0f7685e47bbe7c9f4310bbabd87ac4ef07bd397
Valentinka2222 <xamalyan89@gmail.com>
2022-05-17 14:56:24 +0300

    2

--- lesson4/hometask1/src/Clock.jsx
+++ lesson4/hometask1/src/Clock.jsx
@@ -9,29 +9,35 @@ class Clock extends Component {
 
     this.state = {
       location: props.location,
-      date: new Date(),
+      date: props.date,
     };
     this.updateClock = () => {
+      console.log(this);
       setInterval(() => {
-        this.setState({ date: new Date() });
+        this.setState({ date: this.state.date });
       }, 1000);
     };
+
     this.getTimeWithOffset = () => {
       let stringOffset = '0000';
-      stringOffset =
-        stringOffset.substr(0, 1) + `${props.offset}` + stringOffset.substr(props.offset, 1);
+      if (props.offset > 10) {
+        stringOffset = stringOffset.substr(0, 1) + `${props.offset}` + stringOffset.substr(3);
+      } else {
+        stringOffset = stringOffset.substr(0, 1) + `${props.offset}` + stringOffset.substr(2);
+      }
+      const timeRegion = moment.utc(props.date).utcOffset(`'${stringOffset}'`).format('LTS');
 
-      const date = moment.utc(this.state.date).utcOffset(`'${stringOffset}'`).format('LTS');
-      return date;
+      return timeRegion;
     };
   }
 
   render() {
+
     this.updateClock();
     return (
       <div className="clock">
         <div className="clock__location">{`${this.state.location}`}</div>
-        <div className="clock__time">{this.getTimeWithOffset()}</div>
+        <div className="clock__time">{`${this.getTimeWithOffset()}`}</div>
       </div>
     );
   }
