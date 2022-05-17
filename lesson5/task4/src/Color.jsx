1fa10dc5dca0c3cbc82b91e6c5d4b58f48d60095
Valentinka2222 <xamalyan89@gmail.com>
2022-05-17 20:39:50 +0300

    L5t4

---
+++ lesson5/task4/src/Color.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+const RED = '#f00';
+const GREEN = '#0f0';
+const BLUE = '#00f';
+
+class Colors extends Component {
+  setBodyColor = color => {
+    document.body.style.background = color;
+  };
+
+  render() {
+    return (
+      <div className="colors">
+        <button
+          className="colors__button"
+          style={{
+            backgroundColor: RED,
+          }}
+          onClick={() => this.setBodyColor(RED)}
+        ></button>
+        <button
+          className="colors__button"
+          style={{
+            backgroundColor: GREEN,
+          }}
+          onClick={() => this.setBodyColor(GREEN)}
+        ></button>
+        <button
+          className="colors__button"
+          style={{
+            backgroundColor: BLUE,
+          }}
+          onClick={() => this.setBodyColor(BLUE)}
+        ></button>
+      </div>
+    );
+  }
+}
+export default Colors;
---
+++ lesson5/task4/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Colors from './Color';
+
+const rootelement = document.querySelector('#root');
+
+ReactDOM.render(<Colors />, rootelement);
