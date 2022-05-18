fda3ba3595cb5e095f707022c72e585de28abe2e
Valentinka2222 <xamalyan89@gmail.com>
2022-05-18 13:33:39 +0300

    1

--- lesson5/hometask2/src/ColorPicker.jsx
+++ lesson5/hometask2/src/ColorPicker.jsx
@@ -1,22 +1,9 @@
 import React, { Component } from 'react';
 
 class ColorPicker extends Component {
-  constructor(props) {
-    super();
-    this.onMouseEnter = e => {
-      const elementButtonCoral = document.querySelector('.picker__button_coral');
-      const elementButtonAuqa = document.querySelector('.picker__button_aqua');
-      const elementButtonBisque = document.querySelector('.picker__button_bisque');
-      elementButtonAuqa.setAttribute('value', 'Aqua');
-      elementButtonCoral.setAttribute('value', 'Coral');
-      elementButtonBisque.setAttribute('value', 'Bisque');
-      const elementColorTitle = document.querySelector('.picker__title');
-      elementColorTitle.textContent = e.target.value;
-    };
-    this.onMouseLeave = () => {
-      const elementColorTitle = document.querySelector('.picker__title');
-      elementColorTitle.textContent = '';
-    };
+  handleClick(e) {
+    const elementColorTitle = document.querySelector('.picker__title');
+    elementColorTitle.textContent = e.target.value;
   }
 
   render() {
@@ -26,18 +13,18 @@ class ColorPicker extends Component {
         <div>
           <button
             className="picker__button picker__button_coral"
-            onMouseEnter={this.onMouseEnter}
-            onMouseLeave={this.onMouseLeave}
+            value="Coral"
+            onMouseEnter={this.handleClick}
           ></button>
           <button
             className="picker__button picker__button_aqua"
-            onMouseEnter={this.onMouseEnter}
-            onMouseLeave={this.onMouseLeave}
+            onMouseEnter={this.handleClick}
+            value="Aqua"
           ></button>
           <button
             className="picker__button picker__button_bisque"
-            onMouseEnter={this.onMouseEnter}
-            onMouseLeave={this.onMouseLeave}
+            onMouseEnter={this.handleClick}
+            value="Bisque"
           ></button>
         </div>
       </div>
