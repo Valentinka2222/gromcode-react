e8a9d916f723f98cbd3f2e93b058be91c33a92b5
Valentinka2222 <xamalyan89@gmail.com>
2022-05-19 16:46:29 +0300

    2

--- lesson6/task1/src/UserGreeting.jsx
+++ lesson6/task1/src/UserGreeting.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
 const UserGreeting = () => {
-  return <h1>Hello, user {'\u1f60e'}</h1>;
+  return <h1>Hello,user {'\u1f60e'}</h1>;
 };
 export default UserGreeting;
