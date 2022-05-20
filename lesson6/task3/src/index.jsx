f276a8676d0bc05c1932be6139659e598843f85d
Valentinka2222 <xamalyan89@gmail.com>
2022-05-20 15:41:57 +0300

    L6t3

---
+++ lesson6/task3/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Page from './Page';
+
+const rootelement = document.querySelector('#root');
+
+ReactDOM.render(<Page />, rootelement);
