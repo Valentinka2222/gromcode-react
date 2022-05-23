e91f460f641fe0da1cff1aa8cd0e8a74bbafb12b
Valentinka2222 <xamalyan89@gmail.com>
2022-05-23 12:20:50 +0300

    7

--- lesson6/task1/src/GuestGreeting.jsx
+++ lesson6/task1/src/GuestGreeting.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
 const GuestGreeting = () => {
-  return <h1 className="emoji-raceta">Hi Stranger. Please login <span role="img">🚀</span> </h1>;
+  return <h1 className="emoji-raceta">Hi Stranger. Please login 🚀</h1>;
 };
 export default GuestGreeting;
--- lesson6/task1/src/UserGreeting.jsx
+++ lesson6/task1/src/UserGreeting.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
 const UserGreeting = () => {
-  return <h1 className="emoji-smile">Hello, user  <span role='img'>😎</span></h1>;
+  return <h1 className="emoji-smile">Hello, user 😎</h1>;
 };
 export default UserGreeting;
