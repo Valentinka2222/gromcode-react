52cca3aa4a6e380c68f7b5477e7007de24477962
Valentinka2222 <xamalyan89@gmail.com>
2022-05-28 22:52:00 +0300

    3

--- lesson7/hometask1/src/Transaction.jsx
+++ lesson7/hometask1/src/Transaction.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import moment from 'moment';
 
-const formatTime = time => moment(time).format('HH:mm');
-const formatDate = () => moment().locale('en').format('DD MMMM');
 const formatNumber = number => new Intl.NumberFormat('en-GB').format(number);
 
 const Transaction = ({ from, to, amount, rate, time }) => {
   return (
     <li className="transaction">
-      <span className="transaction__date">{formatDate()}</span>
-      <span className="transaction__time">{formatTime(time)}</span>
+      <span className="transaction__date">{moment().locale('en').format('DD MMMM')}</span>
+      <span className="transaction__time">{moment(time).format('HH:mm')}</span>
       <span className="transaction__assets">
         {from} → {to}
       </span>
