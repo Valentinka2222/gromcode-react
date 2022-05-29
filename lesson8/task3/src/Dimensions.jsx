380a3c33b030a3a14f88c9aa5cb96916124cc023
Valentinka2222 <xamalyan89@gmail.com>
2022-05-29 16:28:03 +0300

    L8t3

---
+++ lesson8/task3/src/Dimensions.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class Dimensions extends Component {
+  state = {
+    width: null,
+    height: null,
+  };
+
+  componentDidMount() {
+    window.addEventListener('resize', this.onResize);
+
+    const { innerWidth, innerHeight } = window;
+    this.setDimention(innerHeight, innerWidth);
+  }
+  componenWillUnMount() {
+    window.removeEventListener('resize', this.onResize);
+  }
+  onResize = e => {
+    const { innerWidth, innerHeight } = e.target;
+
+    this.setDimention(innerHeight, innerWidth);
+  };
+
+  setDimention = (width, height) => {
+    this.setState({
+      width,
+      height,
+    });
+  };
+
+  render() {
+    return <div className="dimensions">{`${this.state.width}px - ${this.state.height}px`}</div>;
+  }
+}
+export default Dimensions;
---
+++ lesson8/task3/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+
+import Dimensions from './Dimensions';
+
+const rootelement = document.querySelector('#root');
+
+ReactDOM.render(<Dimensions />, rootelement);
