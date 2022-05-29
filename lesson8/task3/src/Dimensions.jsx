77dfe762afc1082120307018d3be35001366042e
Valentinka2222 <xamalyan89@gmail.com>
2022-05-29 17:04:39 +0300

    1

--- lesson8/task3/src/Dimensions.jsx
+++ lesson8/task3/src/Dimensions.jsx
@@ -12,7 +12,7 @@ class Dimensions extends Component {
     const { innerWidth, innerHeight } = window;
     this.setDimensions(innerHeight, innerWidth);
   }
-  componentWillUnMount() {
+  componentWillUnmount() {
     window.removeEventListener('resize', this.onResize);
   }
   onResize = e => {
