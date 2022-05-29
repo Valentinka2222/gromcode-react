465f0357cfe0849f8eee611da1987d102ca851ac
Valentinka2222 <xamalyan89@gmail.com>
2022-05-29 16:29:39 +0300

    1

--- lesson8/task3/src/Dimension.jsx
+++ lesson8/task3/src/Dimension.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class Dimensions extends Component {
+class Dimension extends Component {
   state = {
     width: null,
     height: null,
@@ -32,4 +32,4 @@ class Dimensions extends Component {
     return <div className="dimensions">{`${this.state.width}px - ${this.state.height}px`}</div>;
   }
 }
-export default Dimensions;
+export default Dimension;
