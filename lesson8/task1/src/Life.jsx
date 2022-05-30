dcd8c7cc31f3787834284ab22c3b9f226c6ac966
Valentinka2222 <xamalyan89@gmail.com>
2022-05-30 11:14:28 +0300

    7

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -4,7 +4,7 @@ class Life extends Component {
   constructor(props) {
     super(props);
     
-    console.log('constructor: good place to create state');
+
   }
   componentDidMount() {
     console.log('componentDidMount: API calls, subscriptions');
@@ -16,9 +16,7 @@ class Life extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-   
-    console.log('constructor: good place to create state');
-    console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
+ console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
   }
 
   componentWillUnmount() {
@@ -26,7 +24,9 @@ class Life extends Component {
   }
 
   render() {
+  console.log('constructor: good place to create state');
     console.log('return React element to build DOM');
+   
     return <div>{this.props.number}</div>;
   }
 }
