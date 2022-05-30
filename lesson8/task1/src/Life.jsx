552e512b9dd1cefab8ca5d681d941c41fc3fd388
Valentinka2222 <xamalyan89@gmail.com>
2022-05-30 10:59:04 +0300

    8

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -3,19 +3,25 @@ import React, { Component } from 'react';
 class Life extends Component {
   constructor(props) {
     super(props);
+    
     console.log('constructor: good place to create state');
   }
   componentDidMount() {
     console.log('componentDidMount: API calls, subscriptions');
   }
-  componentDidUpdate(prevProps, prevState) {
-   console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
-  }
-shouldComponentUpdate(nextProps, nextState) {
+
+  shouldComponentUpdate(nextProps, nextState) {
     console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
     return nextProps.number % 2;
   }
-componentWillUnmount() {
+
+  componentDidUpdate(prevProps, prevState) {
+   
+    console.log('constructor: good place to create state');
+    console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
+  }
+
+  componentWillUnmount() {
     console.log('componentWillUnmount(): cleanup before DOM related to component will be removed');
   }
 
