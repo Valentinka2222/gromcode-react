932f2504749b590a1b47b11ec848cf75dbe9397e
Valentinka2222 <xamalyan89@gmail.com>
2022-05-30 12:48:43 +0300

    L9t1

---
+++ lesson9/task1/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Search from './Search';
+
+const rootelement = document.querySelector('#root');
+
+ReactDOM.render(<Search />, rootelement);
