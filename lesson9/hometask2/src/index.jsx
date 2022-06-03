d31474d48166b22c202af6352c7d939c139111cf
Valentinka2222 <xamalyan89@gmail.com>
2022-06-03 11:15:31 +0300

    L10t1

--- lesson9/hometask2/src/index.jsx
+++ lesson9/hometask2/src/index.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import Game from './Game';
+const root = document.querySelector('#root');
 
 ReactDOM.render(<Game />, root);
