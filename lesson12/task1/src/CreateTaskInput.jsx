ec1f76b74f6aed366f70c37192d507ef9560c7ba
Valentinka2222 <xamalyan89@gmail.com>
2022-06-06 21:08:04 +0300

    L12t1

---
+++ lesson12/task1/src/CreateTaskInput.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class CreateTaskInput extends Component {
+  state = {
+    value: '',
+  };
+  handleChange = event => {
+    this.setState({
+      value: event.target.value,
+    });
+  };
+  handleTaskCreate = () => {
+    this.props.onCreate(this.state.value);
+    this.setState({ value: '' });
+  };
+  render() {
+    return (
+      <div className="create-task">
+        <input
+          type="text"
+          value={this.state.value}
+          className="create-task__input"
+          onChange={this.handleChange}
+        />
+        <button className="btn create-task__btn" onClick={this.handleTaskCreate}>
+          Create
+        </button>
+      </div>
+    );
+  }
+}
+CreateTaskInput.propTypes = {
+  value: PropTypes.string,
+};
+CreateTaskInput.defaultProps = {
+  value: '',
+};
+export default CreateTaskInput;
