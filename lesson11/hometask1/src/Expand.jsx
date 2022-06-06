476aac9748254d4fcd18ac7716f81616bb6d8fd3
Valentinka2222 <xamalyan89@gmail.com>
2022-06-06 12:13:34 +0300

    0

--- lesson11/hometask1/src/Expand.jsx
+++ lesson11/hometask1/src/Expand.jsx
@@ -1,23 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 const Expand = ({ isHide, title, onClose, arrow, children }) => {
-  let elementContent;
-  if (isHide) {
-    elementContent = null;
-  } else {
-    elementContent = <div className="expand__content">{children}</div>;
-  }
   return (
     <div className="expand border">
       <div className="expand__header">
         <span className="expand__title">{title}</span>
-
         <button className="expand__toggle-btn" onClick={onClose}>
           <i className={`fas ${arrow} `}></i>
         </button>
       </div>
-      {elementContent}
+      {!isHide ? <div className="expand__content">{children}</div> : null}
     </div>
   );
 };
