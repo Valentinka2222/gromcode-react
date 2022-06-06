4365cabeba19fb8474c95fa1a695022ce357e4a2
Valentinka2222 <xamalyan89@gmail.com>
2022-06-06 11:59:16 +0300

    3

--- lesson11/hometask1/src/App.jsx
+++ lesson11/hometask1/src/App.jsx
@@ -11,17 +11,17 @@ class App extends Component {
     });
   };
   render() {
-    let elem = (
-      <p>
-        Hooks are a new addition in React 16.8. They let you use state and other React features
-        without writing a class.
-      </p>
-    );
-    if (this.state.isHide) {
-      this.arrow = 'fa-chevron-down';
-      elem = <></>;
-    } else {
+    let elem = null;
+    if (!this.state.isHide) {
       this.arrow = 'fa-chevron-up';
+      elem = (
+        <p>
+          Hooks are a new addition in React 16.8. They let you use state and other React features
+          without writing a class.
+        </p>
+      );
+    } else {
+      this.arrow = 'fa-chevron-down';
     }
     return (
       <div className="app">
