58f467829100f7b5dcfd670f99ef67894472b4ea
Valentinka2222 <xamalyan89@gmail.com>
2022-06-09 13:53:51 +0300

    L14t2

--- lesson14/task1/src/Counter.jsx
+++ lesson14/task1/src/Counter.jsx
@@ -25,45 +25,4 @@ const Counter = () => {
   );
 };
 
-// class Counter extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       counter: 0,
-//     };
-//     this.decrement = () => {
-//       this.setState({
-//         counter: this.state.counter - 1,
-//       });
-//     };
-//     this.increment = this.increment.bind(this);
-//   }
-
-//   increment() {
-//     this.setState({
-//       counter: this.state.counter + 1,
-//     });
-//   }
-//   reset = () => {
-//     this.setState({
-//       counter: 0,
-//     });
-//   };
-
-//   render() {
-//     return (
-//       <div className="counter">
-//         <button dataAction="decrease" className="counter__button" onClick={this.decrement}>
-//           -
-//         </button>
-//         <span className="counter__value" onClick={() => this.reset()}>
-//           {this.state.counter}
-//         </span>
-//         <button dataAction="increase" className="counter__button" onClick={this.increment}>
-//           +
-//         </button>
-//       </div>
-//     );
-//   }
-// }
 export default Counter;
