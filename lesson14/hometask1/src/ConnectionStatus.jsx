57cf41c1ee0e68829694eb77ce1d87ac27fc502a
Valentinka2222 <xamalyan89@gmail.com>
2022-06-10 14:17:51 +0300

    1

--- lesson14/hometask1/src/ConnectionStatus.jsx
+++ lesson14/hometask1/src/ConnectionStatus.jsx
@@ -1,19 +1,18 @@
-
 import React, { useState, useEffect } from 'react';
 
 const ConnectionStatus = () => {
   const [isOnline, setIsOnline] = useState(true);
   useEffect(() => {
-    setIsOnline((prev) => {
-     
-      if (!prev) { return false };
-      return true
-     });
-      window.addEventListener('online', (isOnline) => setIsOnline(isOnline));
-      window.addEventListener('offline', () => setIsOnline());
-    window.removeEventListener('online', (isOnline) => setIsOnline(isOnline));
-     window.removeEventListener('offline', () => setIsOnline());
-    
+    setIsOnline(prev => {
+      if (!prev) {
+        return false;
+      }
+      return true;
+    });
+    window.addEventListener('online', isOnline => setIsOnline(isOnline));
+    window.addEventListener('offline', () => setIsOnline());
+    window.removeEventListener('online', isOnline => setIsOnline(isOnline));
+    window.removeEventListener('offline', () => setIsOnline());
   }, [isOnline]);
   return (
     <div className={isOnline ? 'status' : 'status status_offline'}>
